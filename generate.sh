521586f0589ff67d4cfbb3f93d4ee6b58bb71c8c
lazos <lazos@lazos.me>
2025-05-24 02:48:46 +0000

    fixup

--- generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Simple wrapper script for make commands to generate Protocol Buffer files
-
-# Exit on error
-set -e
-
-echo "Generating Protocol Buffer files..."
-make clean && make all
-echo "Generation complete!"
