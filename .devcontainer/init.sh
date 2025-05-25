a70b1efba4193ea6748b885cf6cef4618321574f
lazos <lazos@lazos.me>
2025-05-25 18:47:57 +0000

    devcontainer

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env sh
 
-echo "Creating .env file"
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-cat <<EOF > .devcontainer/.env
+echo "Creating .env file with environment variables..."
+
+if [ ! -f "${SCRIPT_DIR}/.env" ]; then
+    cat <<EOL > "${SCRIPT_DIR}/.env"
 USERNAME=$(id -un)
 USER_UID=$(id -u)
 USER_GID=$(id -g)
-EOF
+EOL
+    echo ".env file created."
+fi
